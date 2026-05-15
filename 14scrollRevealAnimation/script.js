a1b0c65910bd7001cf534d896bd6c1485bc3d5ab
AnkitVS0702 <ankitshiksharthi@gmail.com>
2026-05-15 14:56:28 +0530

    Made a website which reveals animation on scrolling

---
+++ 14scrollRevealAnimation/script.js
@@ -0,0 +1,22 @@
+const reveals = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver((entries) => {
+
+      entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+          entry.target.classList.add("show");
+        }
+        else{
+          entry.target.classList.remove("show");
+        }
+
+      });
+
+    }, {
+      threshold: 0.2
+    });
+
+    reveals.forEach((element) => {
+      observer.observe(element);
+    });
